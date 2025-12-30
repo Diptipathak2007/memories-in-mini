4f995688adb056815c6e019614af8f9eb119bd88
Diptipathak2007 <diptipathak019@gmail.com>
2025-12-30 18:25:50 +0530

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,8 @@ export const metadata: Metadata = {
   description: "Turning your precious moments into timeless Polaroids. Custom Polaroid photo prints crafted with care.",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/logo.png",
+    apple: "/logo.png",
   },
 }
 
